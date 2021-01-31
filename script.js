b03d3d481bb46fc450d45f0f34a20b2e8328c467
RudroMajumder <majumder.rudrofficial@gmail.com>
2021-01-31 21:54:33 +0600

    pin

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 
 function generate(){
-    const generate = Math.ceil(Math.random()*10000);
-    document.getElementById('generate-display').value = generate;
-    return generate;
+    const generatePin = Math.round(Math.random()*10000);
+    document.getElementById('generate-display').value = generatePin;
+    const pinLength = generatePin.toString();
+    if( pinLength.length < 4 ){
+        generate();
+    }
+    document.getElementById('display').value = "";
+    return generatePin;
 }
+
 function display(value){
-   const display =  document.getElementById('display').value += value;
-   return display;
-}
+    const displayPin =  document.getElementById('display').value += value;
+    return displayPin;
+ }
+
 function remove(){
     const item = document.getElementById('value-c').innerText;
     if( item === 'C') {
@@ -17,17 +24,27 @@ function remove(){
     document.getElementById('wrong').style.display = 'none';
     document.getElementById('verified').style.display = 'none';
 }
+
+function backspace() {
+    const input = document.getElementById('display').value;
+    const text = input.toString();
+    const editedText = text.slice(0, -1);
+    document.getElementById('display').value = editedText;
+}
+
 document.getElementById('wrong').style.display = 'none';
 document.getElementById('verified').style.display = 'none';
+
 function submit(display) {
     const generatedPin = document.getElementById('generate-display').value; 
     const typedPin = display;
     const pin = parseInt(typedPin);
-    let tryLeft = document.getElementById('try-left').innerText;
     if( generatedPin == pin ){
         document.getElementById('verified').style.display = 'flex';
+        document.getElementById('wrong').style.display = 'none';
     }
     else{
         document.getElementById('wrong').style.display = 'flex';
+        document.getElementById('display').value = "";
     }
 }
